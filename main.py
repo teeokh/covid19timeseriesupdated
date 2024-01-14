e26cd58608b7d6d5030cff39f4667a7964062419
Tee <139244119+teeokh@users.noreply.github.com>
2024-01-14 16:38:40 +1000

    Import dataset

--- main.py
+++ main.py
@@ -2,3 +2,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from statsmodels.tsa.stattools import adfuller
+
+
+def test_stationarity(timeseries):  # Tests the stationarity of time series using ADF test
+    rolmean = timeseries.rolling(window=5).mean()
+    rolstd = timeseries.rolling(window=5).std()
+
+    orig = plt.plot(timeseries, label="Original")
+    mean = plt.plot(timeseries, label="Rolling Mean")
+    std = plt.plot(timeseries, label="Rolling std")
+
+    plt.legend(loc="best")
+    plt.title("Timeseries data with rolling mean and std")
+    plt.show()
+
+    dftest = adfuller(timeseries)
+    dfoutput = pd.Series(dftest[0:4],
+                         index=["The test statistic", "Mackinnon's approximate p-value", "#usedLags", "NOBS"])
+
+    print(dfoutput)
+
+
+dtf = '/Users/terrellokhiria/PycharmProjects/tensorEnv/covid19timeseriesupdated/covid_19_data.csv'
+
+covid_19_dtf = pd.read_csv(dtf)
+print(covid_19_dtf.head())
