e8b1833f0deec3295ad3b4ac30634fc088d7d1bb
Tee <139244119+teeokh@users.noreply.github.com>
2024-01-14 17:42:27 +1000

    First ACF and PACF scores

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from statsmodels.tsa.stattools import adfuller
-
+from statsmodels.tsa.stattools import acf
+from statsmodels.tsa.stattools import pacf
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.graphics.tsaplots import plot_pacf
 
 def test_stationarity(timeseries):  # Tests the stationarity of time series using ADF test
     rolmean = timeseries.rolling(window=5).mean()
@@ -37,12 +40,23 @@ covid_UK_dataset = idx_covid_19_dtf[covid_data_UK]
 covid_UK_confirmed_case_data = covid_UK_dataset['Confirmed']
 covid_UK_confirmed_agg_data = covid_UK_confirmed_case_data.groupby(['ObservationDate']).sum()
 
-test_stationarity(covid_UK_confirmed_agg_data)
+#test_stationarity(covid_UK_confirmed_agg_data)
 
 covid_UK_confirmed_agg_data_log = np.log(covid_UK_confirmed_agg_data)
-test_stationarity(covid_UK_confirmed_agg_data_log)
+#test_stationarity(covid_UK_confirmed_agg_data_log)
 
 ma = covid_UK_confirmed_agg_data_log.rolling(window=5).mean()
 covid_UK_confirmed_agg_data_log_minus_ma = covid_UK_confirmed_agg_data_log - ma
 covid_UK_confirmed_agg_data_log_minus_ma.dropna(inplace=True)
-test_stationarity(covid_UK_confirmed_agg_data_log_minus_ma)
+#test_stationarity(covid_UK_confirmed_agg_data_log_minus_ma)
+
+
+lag_acf = acf(covid_UK_confirmed_agg_data_log_minus_ma, nlags=32)
+lag_pacf = pacf(covid_UK_confirmed_agg_data_log_minus_ma, nlags=16)
+
+fig, ax = plt.subplots(1,2, figsize=(20,5))
+
+plot_acf(lag_acf, ax=ax[0])
+plot_pacf(lag_pacf, lags=7, ax=ax[1])
+
+plt.show()
